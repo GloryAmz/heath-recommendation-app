077a81977fc3448c25122f031124ec4622a0d7e5
GloryAmz <chimamakaogbuagu@gmail.com>
2024-11-15 15:30:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
 
 st.title('Health Recommendation app')
 
@@ -7,7 +9,8 @@ st.write('This is a health prediction app')
 
 with st.expander('Data:'):
   st.write('**Raw Data**')
-df = pd.read_csv('user_data.csv')
+df = pd.read_csv('update_data.csv')
 df
+
 
 
